d7f5a6ebca881ba344bb8c128794b46863c0f51a
b83c <b83c@outlook.com>
2023-05-21 07:23:53 +0800

    Update #2

--- src/main.rs
+++ src/main.rs
@@ -40,9 +40,13 @@ mod app {
     #[shared]
     struct Shared {
         #[lock_free]
-        fbu: Cycle<Flatten<StepBy<ChunksExact<'static, u32>>>>,
+        frame_offset: usize,
         #[lock_free]
-        fbd: Cycle<Flatten<StepBy<Skip<ChunksExact<'static, u32>>>>>,
+        buf: &'static [u32],
+        // #[lock_free]
+        // fbu: Cycle<Flatten<StepBy<ChunksExact<'static, u32>>>>,
+        // #[lock_free]
+        // fbd: Cycle<Flatten<StepBy<Skip<ChunksExact<'static, u32>>>>>,
     }
 
     #[local]
@@ -50,68 +54,6 @@ mod app {
         en: u8,
         port: u8,
         timer7: CounterHz<TIM7>,
-        // timers: (
-        //     PwmHz<
-        //         TIM1,
-        //         (
-        //             Channel1<TIM1, false>,
-        //             Channel2<TIM1, false>,
-        //             Channel3<TIM1, false>,
-        //             Channel4<TIM1, false>,
-        //         ),
-        //     >,
-        //     PwmHz<
-        //         TIM2,
-        //         (
-        //             Channel1<TIM2, false>,
-        //             Channel2<TIM2, false>,
-        //             Channel3<TIM2, false>,
-        //             Channel4<TIM2, false>,
-        //         ),
-        //     >,
-        //     PwmHz<
-        //         TIM3,
-        //         (
-        //             Channel1<TIM3, false>,
-        //             Channel2<TIM3, false>,
-        //             Channel3<TIM3, false>,
-        //             Channel4<TIM3, false>,
-        //         ),
-        //     >,
-        //     PwmHz<
-        //         TIM4,
-        //         (
-        //             Channel1<TIM4, false>,
-        //             Channel2<TIM4, false>,
-        //             Channel3<TIM4, false>,
-        //             Channel4<TIM4, false>,
-        //         ),
-        //     >,
-        //     PwmHz<
-        //         TIM5,
-        //         (
-        //             Channel1<TIM5, false>,
-        //             Channel2<TIM5, false>,
-        //             Channel3<TIM5, false>,
-        //             Channel4<TIM5, false>,
-        //         ),
-        //     >,
-        //     PwmHz<
-        //         TIM8,
-        //         (
-        //             Channel1<TIM8, false>,
-        //             Channel2<TIM8, false>,
-        //             Channel3<TIM8, false>,
-        //             Channel4<TIM8, false>,
-        //         ),
-        //     >,
-        //     PwmHz<TIM9, (Channel1<TIM9, false>, Channel2<TIM9, false>)>,
-        //     PwmHz<TIM10, Channel1<TIM10, false>>,
-        //     PwmHz<TIM11, Channel1<TIM11, false>>,
-        //     PwmHz<TIM12, (Channel1<TIM12, false>, Channel2<TIM12, false>)>,
-        //     PwmHz<TIM13, Channel1<TIM13, false>>,
-        //     PwmHz<TIM14, Channel1<TIM14, false>>,
-        // ),
     }
 
     /// STM32 Init code
@@ -235,12 +177,9 @@ mod app {
             &clocks,
         );
 
-        _ = gpiod.pd0.into_push_pull_output();
-        _ = gpiod.pd1.into_push_pull_output();
-        _ = gpiod.pd2.into_push_pull_output();
-        _ = gpiod.pd3.into_push_pull_output();
-        _ = gpiod.pd4.into_push_pull_output();
-        _ = gpiod.pd5.into_push_pull_output();
+        seq!(N in 0..10{
+        _ = gpiod.pd~N.into_push_pull_output();
+        });
 
         let mut tim7 = dp.TIM7.counter_hz(&clocks);
 
@@ -251,25 +190,15 @@ mod app {
 
         // rtic::pend(hal::pac::Interrupt::TIM7);
 
-        // tim1.enable(hal::timer::Channel::C1);
-
         (
             Shared {
-                fbu: fbpool[0].chunks_exact(128).step_by(2).flatten().cycle(),
-                fbd: fbpool[0]
-                    .chunks_exact(128)
-                    .skip(1)
-                    .step_by(2)
-                    .flatten()
-                    .cycle(),
+                frame_offset: 0,
+                buf: &fbpool[0],
             },
             Local {
                 en: 0,
-                port: 0,
+                port: 0b00000000,
                 timer7: tim7,
-                // timers: (
-                //     tim1, tim2, tim3, tim4, tim5, tim8, tim9, tim10, tim11, tim12, tim13, tim14,
-                // ),
             },
         )
     }
@@ -283,14 +212,16 @@ mod app {
         }
     }
 
-    #[task(binds = TIM7, shared=[fbu, fbd], local=[timer7, port, en])]
+    #[task(binds = TIM7, shared=[frame_offset, &buf], local=[timer7, port, en])]
     fn frame_update(ctx: frame_update::Context) {
         // rprintln!("Update");
-        // let (t1, t2, t3, t4, t5, t8, t9, t10, t11, t12, t13, t14) = ctx.local.timers;
-        let en = ctx.local.en;
-        let port = ctx.local.port;
-        let fbu = ctx.shared.fbu;
-        let fbd = ctx.shared.fbd;
+
+        let en = *ctx.local.en;
+        let port = *ctx.local.port;
+        let frame_offset = *ctx.shared.frame_offset;
+
+        // rprintln!("{} {:#08b} {}", frame_offset, port, en);
+        let buf = ctx.shared.buf;
 
         let timers = (
             TIM1::ptr(),
@@ -314,130 +245,36 @@ mod app {
 
             (*GPIOD::ptr())
                 .odr
-                .modify(|_, w| w.bits(((0b1000 << *en) | *port & 0b111) as u32));
+                .modify(|_, w| w.bits(((0b10000000 << (en >> 3)) | port & 0b1111111) as u32));
+
+            let buf = *buf;
+            let mut fb = buf[frame_offset..(frame_offset + 16)].iter();
 
             seq!(N in 0..4 {
                 (*timers.N).ccr.iter().for_each(|x| {
-                    let temp = ((*fbu.next().unwrap_unchecked() >> *en) as u8);
-                    rdbg!(&temp);
+                    let temp = (*fb.next().unwrap_unchecked() >> en) as u8;
+                    // rdbg!(&temp);
                     x.write(|w| w.ccr().bits(temp.into()))
                 });
             });
 
-            // let mut chunk = fb.skip(7).next().unwrap_unchecked().iter().take(16);
+            let frame_offset = frame_offset + 128;
+            let mut fb = buf[frame_offset..(frame_offset + 16)].iter();
 
             seq!(N in 4..12 {
                 (*timers.N).ccr.iter().for_each(|x| {
-                    x.write(|w| w.ccr().bits(((*fbd.next().unwrap_unchecked() >> *en) as u8).into()))
+                    x.write(|w| w.ccr().bits(((*fb.next().unwrap_unchecked() >> en) as u8).into()))
                 });
             });
         }
 
-        rdbg!(*en);
-        *en = if *en < 2 { *en + 1 } else { 0 };
-        *port += 1;
-
-        // iter1.
-        // unsafe {
-        //     let iter1 = (*TIM1::ptr()).ccr.iter();
-        //     let iter2 = (*TIM2::ptr()).ccr.iter();
-        //     let iter3 = (*TIM3::ptr()).ccr.iter();
-        //     let iter4 = (*TIM4::ptr()).ccr.iter();
-
-        // let test = itertools::chain!(iter1, iter2);
-        // iter1.chain(iter2).enumerate().for_each(|(i, x)| {
-        //     x.write(|w| w.ccr().bits(((fb[off + 0 + i] >> en) as u8).into()))
-        // });
-        // unsafe {
-        //     let en = *en;
-        //     let off = *off as usize;
-        //     (*TIM1::ptr()).ccr.iter().enumerate().for_each(|(i, x)| {
-        //         x.write(|w| w.ccr().bits(((fb[off + 0 + i] >> en) as u8).into()))
-        //     });
-        //     (*TIM2::ptr()).ccr.iter().enumerate().for_each(|(i, x)| {
-        //         x.write(|w| w.ccr().bits(((fb[off + 4 + i] >> en) as u8).into()))
-        //     });
-        //     (*TIM3::ptr()).ccr.iter().enumerate().for_each(|(i, x)| {
-        //         x.write(|w| w.ccr().bits(((fb[off + 8 + i] >> en) as u8).into()))
-        //     });
-        //     (*TIM4::ptr()).ccr.iter().enumerate().for_each(|(i, x)| {
-        //         x.write(|w| w.ccr().bits(((fb[off + 12 + i] >> en) as u8).into()))
-        //     });
-
-        //     let off = off + 128;
-        //     (*TIM5::ptr()).ccr.iter().enumerate().for_each(|(i, x)| {
-        //         x.write(|w| w.ccr().bits(((fb[off + 0 + i] >> en) as u8).into()))
-        //     });
-        //     (*TIM8::ptr()).ccr.iter().enumerate().for_each(|(i, x)| {
-        //         x.write(|w| w.ccr().bits(((fb[off + 4 + i] >> en) as u8).into()))
-        //     });
-        //     (*TIM9::ptr()).ccr.iter().enumerate().for_each(|(i, x)| {
-        //         x.write(|w| w.ccr().bits(((fb[off + 8 + i] >> en) as u8).into()))
-        //     });
-        //     (*TIM10::ptr()).ccr.iter().enumerate().for_each(|(i, x)| {
-        //         x.write(|w| w.ccr().bits(((fb[off + 12 + i] >> en) as u8).into()))
-        //     });
-        //     (*TIM11::ptr()).ccr.iter().enumerate().for_each(|(i, x)| {
-        //         x.write(|w| w.ccr().bits(((fb[off + 12 + i] >> en) as u8).into()))
-        //     });
-        //     (*TIM12::ptr()).ccr.iter().enumerate().for_each(|(i, x)| {
-        //         x.write(|w| w.ccr().bits(((fb[off + 12 + i] >> en) as u8).into()))
-        //     });
-        //     (*TIM13::ptr()).ccr.iter().enumerate().for_each(|(i, x)| {
-        //         x.write(|w| w.ccr().bits(((fb[off + 12 + i] >> en) as u8).into()))
-        //     });
-        //     (*TIM12::ptr()).ccr.iter().enumerate().for_each(|(i, x)| {
-        //         x.write(|w| w.ccr().bits(((fb[off + 12 + i] >> en) as u8).into()))
-        //     });
-        // }
-
-        // {
-        //     let en = *en;
-        //     let port = *port;
-        //     let offset = *off;
-        //     unsafe {
-        //         (*GPIOD::ptr())
-        //             .odr
-        //             .modify(|_, w| w.bits(((en << 3) | port & 0b111) as u32));
-        //     }
-        //     t1.set_duty(C1, ((fb[offset + 0] >> en) as u8) as u16);
-        //     t1.set_duty(C2, ((fb[offset + 1] >> en) as u8) as u16);
-        //     t1.set_duty(C3, ((fb[offset + 2] >> en) as u8) as u16);
-        //     t1.set_duty(C4, ((fb[offset + 3] >> en) as u8) as u16);
-        //     t2.set_duty(C1, ((fb[offset + 4] >> en) as u8) as u16);
-        //     t2.set_duty(C2, ((fb[offset + 5] >> en) as u8) as u16);
-        //     t2.set_duty(C3, ((fb[offset + 6] >> en) as u8) as u16);
-        //     t2.set_duty(C4, ((fb[offset + 7] >> en) as u8) as u16);
-        //     t3.set_duty(C1, ((fb[offset + 8] >> en) as u8) as u16);
-        //     t3.set_duty(C2, ((fb[offset + 9] >> en) as u8) as u16);
-        //     t3.set_duty(C3, ((fb[offset + 10] >> en) as u8) as u16);
-        //     t3.set_duty(C4, ((fb[offset + 11] >> en) as u8) as u16);
-        //     t4.set_duty(C1, ((fb[offset + 12] >> en) as u8) as u16);
-        //     t4.set_duty(C2, ((fb[offset + 13] >> en) as u8) as u16);
-        //     t4.set_duty(C3, ((fb[offset + 14] >> en) as u8) as u16);
-        //     t4.set_duty(C4, ((fb[offset + 15] >> en) as u8) as u16);
-
-        //     let offset = offset + 128;
-
-        //     t5.set_duty(C1, ((fb[offset + 0] >> en) as u8) as u16);
-        //     t5.set_duty(C2, ((fb[offset + 1] >> en) as u8) as u16);
-        //     t5.set_duty(C3, ((fb[offset + 2] >> en) as u8) as u16);
-        //     t5.set_duty(C4, ((fb[offset + 3] >> en) as u8) as u16);
-        //     t8.set_duty(C1, ((fb[offset + 4] >> en) as u8) as u16);
-        //     t8.set_duty(C2, ((fb[offset + 5] >> en) as u8) as u16);
-        //     t8.set_duty(C3, ((fb[offset + 6] >> en) as u8) as u16);
-        //     t8.set_duty(C4, ((fb[offset + 7] >> en) as u8) as u16);
-        //     t9.set_duty(C1, ((fb[offset + 8] >> en) as u8) as u16);
-        //     t9.set_duty(C2, ((fb[offset + 9] >> en) as u8) as u16);
-        //     t10.set_duty(C1, ((fb[offset + 10] >> en) as u8) as u16);
-        //     t11.set_duty(C1, ((fb[offset + 11] >> en) as u8) as u16);
-        //     t12.set_duty(C1, ((fb[offset + 12] >> en) as u8) as u16);
-        //     t12.set_duty(C2, ((fb[offset + 13] >> en) as u8) as u16);
-        //     t13.set_duty(C1, ((fb[offset + 14] >> en) as u8) as u16);
-        //     t14.set_duty(C1, ((fb[offset + 15] >> en) as u8) as u16);
-        // }
-
-        // *off += 128;
+        *ctx.local.en = en + 8;
+        if en == 16 {
+            *ctx.local.en -= 24;
+            *ctx.local.port += 1;
+            let frame_offset = frame_offset + 16;
+            *ctx.shared.frame_offset = (16 * 16 * 16 - 1) & (frame_offset + (frame_offset & 128));
+        }
 
         ctx.local.timer7.clear_interrupt(Event::Update);
     }
